6a0a1f3d0899b5589b5338cc895be839793ca827
arafet <marnissiarafet@gmail.com>
2024-08-01 10:54:57 +0100

    update library

--- src/cnnClassifier/components/prepare_callbacks.py
+++ src/cnnClassifier/components/prepare_callbacks.py
@@ -25,7 +25,7 @@ class PrepareCallback:
     @property
     def _create_ckpt_callbacks(self):
         return tf.keras.callbacks.ModelCheckpoint(
-            filepath=str(self.config.checkpoint_model_filepath),
+            filepath=self.config.checkpoint_model_filepath,
             save_best_only=True
         )
 
